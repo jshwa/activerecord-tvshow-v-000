df4de3209a0464e642b663e83a0db554a9c2466d
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-30 00:45:27 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,19 @@
 class Show < ActiveRecord::Base
+  
+  def highest_rating
+    Show.maximum(:rating)
+  end
+
+  def most_popular_show
+    Show.find_by(rating: highest_rating)
+  end
+
+  def lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def least_popular_show
+    Show.find_by(rating: lowest_rating)
+  end
+  
 end
