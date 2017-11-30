26bfe941e24a5b1fc9f02fc6f0b2534c36dfd199
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-30 00:48:28 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,4 +16,12 @@ class Show < ActiveRecord::Base
     Show.find_by(rating: lowest_rating)
   end
 
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating < 5")
+  end
+
 end
