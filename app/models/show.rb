4f519953821f9c09a48b208204c46a84504758fa
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-30 00:50:49 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,11 +21,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.popular_shows
-    Show.where("rating < 5")
+    Show.where("rating > ?", 5)
   end
 
   def self.shows_by_alphabetical_order
-    Show.order(title)
+    Show.order(:title)
   end
 
 end
